8ddca20dd696c65b7bcde4693d240c537026e271
sutherlandboswell <sutherlandboswell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-02 02:43:52 +0000

    Two big new features, local storage of thumbnail URLs and a meta box for the Edit Post page
    git-svn-id: https://plugins.svn.wordpress.org/video-thumbnails/tags/0.3@317841 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- video-thumbnails.php
+++ video-thumbnails.php
@@ -5,7 +5,7 @@ Plugin URI: http://sutherlandboswell.com/2010/11/wordpress-video-thumbnails/
 Description: A plugin designed to fetch video thumbnails. Use <code>&lt;?php video_thumbnail(); ?&gt;</code> in a loop to return a URL for the thumbnail of the first video in a post. Currently works with YouTube and Vimeo, and with the JR_embed plugin.
 Author: Sutherland Boswell
 Author URI: http://sutherlandboswell.com
-Version: 0.1.2
+Version: 0.3
 License: GPL2
 */
 /*  Copyright 2010 Sutherland Boswell  (email : sutherland.boswell@gmail.com)
@@ -25,7 +25,7 @@ License: GPL2
 */
 
 // Get Vimeo Thumbnail
-function getVimeoInfo($id, $info = 'thumbnail_medium') {
+function getVimeoInfo($id, $info = 'thumbnail_large') {
     if (!function_exists('curl_init')) die('CURL is not installed!');
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "http://vimeo.com/api/v2/video/$id.php");
@@ -38,8 +38,18 @@ function getVimeoInfo($id, $info = 'thumbnail_medium') {
     return $output;
 };
 
+// Blip.tv Functions
+function getBliptvInfo($id) {
+	$xml = simplexml_load_file("http://blip.tv/file/$id?skin=rss");
+	$result = $xml->xpath("/rss/channel/item/media:thumbnail/@url");
+	foreach($result as $element) {
+	   $thumbnail = $element["url"];
+	}
+	return $thumbnail;
+}
+
 // The Main Event
-function video_thumbnail() {
+function get_video_thumbnail() {
 
 	// Gets the post's content
 	$markup = get_the_content();
@@ -55,32 +65,66 @@ function video_thumbnail() {
 	// If no standard YouTube embed is found, checks for one embedded with JR_embed
 	if(!isset($matches[1])) {
 		preg_match('#\[youtube id=([A-Za-z0-9\-_]+)]#s', $markup, $matches);
-	};
+	}
 	
-	// If we've found a YouTube video ID, output the thumbnail URL
+	// If we've found a YouTube video ID, return the thumbnail URL
 	if(isset($matches[1])) {
-		echo 'http://img.youtube.com/vi/' . $matches[1] . '/0.jpg';
+		return 'http://img.youtube.com/vi/' . $matches[1] . '/0.jpg';
 	}
 	
 	// If we didn't find anything, check for a standard Vimeo embed
 	else {
 		preg_match('#<object[^>]+>.+?http://vimeo.com/moogaloop.swf\?clip_id=([A-Za-z0-9\-_]+)&.+?</object>#s', $markup, $matches);
 		
+		// Find Vimeo embedded with iframe code
+		if(!isset($matches[1])) {
+			preg_match('#http://player.vimeo.com/video/([0-9]+)#s', $markup, $matches);
+		}
+		
 		// If we still haven't found anything, check for Vimeo embedded with JR_embed
 		if(!isset($matches[1])) {
 	    	preg_match('#\[vimeo id=([A-Za-z0-9\-_]+)]#s', $markup, $matches);
-	    };
-	};
+	    }
+
+		// If we still haven't found anything, check for Vimeo URL
+		if(!isset($matches[1])) {
+	    	preg_match('#http://www.vimeo.com/([A-Za-z0-9\-_]+)#s', $markup, $matches);
+	    }
+
+		// If we still haven't found anything, check for Vimeo shortcode
+		if(!isset($matches[1])) {
+	    	preg_match('#\[vimeo clip_id="([A-Za-z0-9\-_]+)"[^>]*]#s', $markup, $matches);
+	    }
 	
-	// Now if we've found a Vimeo ID, let's echo the thumbnail
-	if(isset($matches[1])) {
-		echo getVimeoInfo($matches[1], $info = 'thumbnail_medium');
-	}
+		// Now if we've found a Vimeo ID, let's return the thumbnail
+		if(isset($matches[1])) {
+			$vimeo_thumbnail = getVimeoInfo($matches[1], $info = 'thumbnail_large');
+			if(isset($vimeo_thumbnail)) {
+				return $vimeo_thumbnail;
+			} else {
+				// If we can't find the Vimeo thumbnail, display default
+				return null;
+			}
+		}
 	
-	// If we still don't find anything, display a default thumbnail
-	else {
-		echo plugins_url() . "/video-thumbnails/default.jpg";
+		// If nothing has been found, look for Blip.tv
+		else {
+			preg_match('#http://blip.tv/file/([0-9]+)#s', $markup, $matches);
+			if(isset($matches[1])) {
+				return getBliptvInfo($matches[1]);
+			}
+			else {
+				return null;
+			}
+		}
 	}
+
+};
+
+// Echo thumbnail
+function video_thumbnail() {
+	if( ( $video_thumbnail = get_video_thumbnail() ) == null ) { echo plugins_url() . "/video-thumbnails/default.jpg"; }
+	else { echo $video_thumbnail; }
 };
 
 ?>
