1e0d9d804136f956825ebff1d35da283a20bb4cb
sutherlandboswell <sutherlandboswell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-23 07:15:10 +0000

    Post ID can now be set manually for use outside of the loop
    git-svn-id: https://plugins.svn.wordpress.org/video-thumbnails/tags/0.5.4@325589 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,120 @@
+=== Video Thumbnails ===
+Contributors: sutherlandboswell
+Donate link: http://sutherlandboswell.com/2010/11/wordpress-video-thumbnails/
+Tags: Video, YouTube, Vimeo, Blip.tv, Thumbnails
+Requires at least: 3.0
+Tested up to: 3.0.2
+Stable tag: 0.5.4
+
+Video Thumbnails is a simple plugin that makes it easy to automatically display video thumbnails in your template.
+
+== Description ==
+
+Video Thumbnails makes it simple to display video thumbnails in your templates. Simply use `<?php video_thumbnail(); ?>` in a loop to find and echo the URL of the first video embedded in a post, or use `<?php $video_thumbnail = get_video_thumbnail(); ?>` if you want to return the URL for use as a variable.
+
+Video Thumbnails currently supports:
+
+* YouTube
+* Vimeo
+* Blip.tv
+* [Simple Video Embedder](http://wordpress.org/extend/plugins/simple-video-embedder/)
+* [Vimeo Shortcode](http://blog.esimplestudios.com/2010/08/embedding-vimeo-videos-in-wordpress/)
+* [JR Embed](http://plugins.jakeruston.co.uk/wordpress/wordpress-plugin-jr-embed/)
+
+When using `video_thumbnail()` and no thumbnail is found, a default thumbnail is echoed, which can be changed by replacing the `default.jpg` file found in your `/plugins/video-thumbnails/` directory.
+
+For more advanced users, the `get_video_thumbnail()` function will return null when no thumbnail is found so a conditional statement can be used to detect if a thumbnail is present and decide what to echo. Here's an example of how to only echo a thumbnail when one is found: `<?php if( ( $video_thumbnail = get_video_thumbnail() ) != null ) { echo "<img src='" . $video_thumbnail . "' />"; } ?>`
+
+== Installation ==
+
+1. Upload the `/video-thumbnails/` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Use `<?php video_thumbnail(); ?>` in a loop inside your template to echo the thumbnail URL. Because this is only a URL, you should set it as an image tag's source. For example, `<img src="<?php video_thumbnail(); ?>" />`. If you'd like to return the URL for use in your PHP, use `get_video_thumbnail()`. For example, `<?php $video_thumbnail = get_video_thumbnail(); ?>`.
+
+== Frequently Asked Questions ==
+
+= Why doesn't the thumbnail show up in the meta box on the Edit Post page after I save it? =
+
+This is probably happening because `video_thumbnail()` or `get_video_thumbnail()` has not be used in a loop for that post yet. Try loading a page that calls for the thumbnail then checking the Edit Post page again. This will be fixed soon.
+
+= Can I use the functions outside of a loop? =
+
+Yes, but you must pass the post ID to the function as a parameter. For example: `<?php $id = 25; $thumbnail = get_video_thumbnail($id); if($thumbnail!=null) echo $thumbnail; ?>`
+
+= My video service/embedding plugin isn't included, can you add it? =
+
+If the service allows a way to retrieve thumbnails, I'll do my best to add it.
+
+= I only want a thumbnail when one is found, how would I do this? =
+
+In version 0.2 `get_video_thumbnail()` was added which returns null when no thumbnail is found. This means you can use something like `<?php if( ( $video_thumbnail = get_video_thumbnail() ) != null ) { echo "<img src='" . $video_thumbnail . "' />"; } ?>` to only display a thumbnail when one exists.
+
+== Screenshots ==
+
+1. The Video Thumbnail meta box on the Edit Post page
+
+== Changelog ==
+
+= 0.5.4 =
+* Video thumbnails can be retrieved for a specific post ID by passing a parameter to the `video_thumbnail()` or `get_video_thumbnail()` function. For example: `<?php $id = 25; $thumbnail = get_video_thumbnail($id); if($thumbnail!=null) echo $thumbnail; ?>`
+
+= 0.5.3 =
+* Better support for Vimeo Shortcode (thanks to Darren for the tip)
+
+= 0.5.2 =
+* Added support for [Simple Video Embedder](http://wordpress.org/extend/plugins/simple-video-embedder/)
+
+= 0.5.1 =
+* Added a test to make sure the YouTube thumbnail actually exists. This prevents you from getting that ugly default thumbnail from YouTube.
+
+= 0.5 =
+* Thumbnail URLs are now stored in a custom field with each post, meaning the plugin only has to interact with outside APIs once per post.
+* Added a "Video Thumbnail" meta box to the edit screen for each post, which can be manually set or will be set automatically once `video_thumbnail()` or `get_video_thumbnail()` is called in a loop for that post.
+
+= 0.3 =
+* Added basic support for Blip.tv auto embedded using URLs in this format: http://blip.tv/file/12345
+
+= 0.2.3 =
+* Added support for any Vimeo URL
+
+= 0.2.2 =
+* Added support for [Vimeo Shortcode](http://blog.esimplestudios.com/2010/08/embedding-vimeo-videos-in-wordpress/)
+
+= 0.2.1 =
+* Added support for Vimeo players embedded using an iframe
+
+= 0.2 =
+* Added `get_video_thumbnail()` to return the URL without echoing or return null if no thumbnail is found, making it possible to only display a thumbnail if one is found.
+
+= 0.1.3 =
+* Fixed an issue where no URL was returned when Vimeo's rate limit had been exceeded. The default image URL is now returned, but a future version of the plugin will store thumbnails locally for a better fix.
+
+= 0.1.2 =
+* Fixed a possible issue with how the default image URL is created
+
+= 0.1.1 =
+* Fixed an issue with the plugin directory's name that caused the default URL to be broken
+* Added support for YouTube URLs
+
+= 0.1 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 0.5 =
+This version adds the thumbnail URL to the post's meta data, meaning any outside APIs only have to be queried once per post. Vimeo's rate limit was easily exceeded, so this should fix that problem.
+
+== Known Issues ==
+
+* Thumbnail URLs are not found and stored until `video_thumbnail()` or `get_video_thumbnail()` is called in a loop for that post. Future versions will handle this at the time of publishing.
+* While not really an issue, the current method for only displaying a thumbnail if one is found seems like it could be streamlined for less experienced users, so if you have any suggestions let me know.
+
+== Roadmap ==
+
+This plugin is still very young, and has a future planned as the ultimate plugin for video thumbnails. Here's some of the planned additions:
+
+* An option to save thumbnails as post attachments and as the featured image, giving the plugin out-of-the-box compatibility with themes that use the Featured Image functionality of WordPress (I'd like some feedback on this idea)
+* More comprehensive Blip.tv support
+* More services
+* Broader plugin compatibility
+* Option to display video thumbnails on the admin post list page
---
+++ video-thumbnails.php
@@ -0,0 +1,202 @@
+<?php
+/*
+Plugin Name: Video Thumbnails
+Plugin URI: http://sutherlandboswell.com/2010/11/wordpress-video-thumbnails/
+Description: Make adding video thumbnails to your posts easier and automatic, just add <code>&lt;?php video_thumbnail(); ?&gt;</code> to your loop to get the thumbnail's URL. <code>&lt;?php get_video_thumbnail(); ?&gt;</code> is also available for more advanced users. Currently works with YouTube, Vimeo, and Blip.tv, along with several embedding plugins.
+Author: Sutherland Boswell
+Author URI: http://sutherlandboswell.com
+Version: 0.5.4
+License: GPL2
+*/
+/*  Copyright 2010 Sutherland Boswell  (email : sutherland.boswell@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Get Vimeo Thumbnail
+function getVimeoInfo($id, $info = 'thumbnail_large') {
+    if (!function_exists('curl_init')) die('CURL is not installed!');
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://vimeo.com/api/v2/video/$id.php");
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    $output = unserialize(curl_exec($ch));
+    $output = $output[0][$info];
+    curl_close($ch);
+    return $output;
+};
+
+// Blip.tv Functions
+function getBliptvInfo($id) {
+	$xml = simplexml_load_file("http://blip.tv/file/$id?skin=rss");
+	$result = $xml->xpath("/rss/channel/item/media:thumbnail/@url");
+	$thumbnail = (string) $result[0]['url'];
+	return $thumbnail;
+}
+
+// The Main Event
+function get_video_thumbnail($post_id=null) {
+	
+	// Get the post ID if none is provided
+	if($post_id==null OR $post_id=='') $post_id = get_the_ID();
+	
+	// Check to see if thumbnail has already been found
+	if( ($thumbnail_meta = get_post_meta($post_id, '_video_thumbnail', true)) != '' ) {
+		return $thumbnail_meta;
+	}
+	// If the thumbnail isn't stored in custom meta, fetch a thumbnail
+	else {
+
+		// Gets the post's content
+		$post_array = get_post($post_id); 
+		$markup = $post_array->post_content;
+		$new_thumbnail = null;
+		
+		// Simple Video Embedder Compatibility
+		if(function_exists('p75HasVideo')) {
+			if ( p75HasVideo($post_id) ) {
+			    $markup = p75GetVideo($post_id);
+			}
+		}
+		
+		// Checks for a standard YouTube embed
+		preg_match('#<object[^>]+>.+?http://www.youtube.com/v/([A-Za-z0-9\-_]+).+?</object>#s', $markup, $matches);
+	
+		// Checks for any YouTube URL
+		if(!isset($matches[1])) {
+			preg_match('#http://w?w?w?.?youtube.com/watch\?v=([A-Za-z0-9\-_]+)#s', $markup, $matches);
+		}
+		
+		// If no standard YouTube embed is found, checks for one embedded with JR_embed
+		if(!isset($matches[1])) {
+			preg_match('#\[youtube id=([A-Za-z0-9\-_]+)]#s', $markup, $matches);
+		}
+		
+		// If we've found a YouTube video ID, create the thumbnail URL
+		if(isset($matches[1])) {
+			$youtube_thumbnail = 'http://img.youtube.com/vi/' . $matches[1] . '/0.jpg';
+			
+			// Check to make sure it's an actual thumbnail
+			$ch = curl_init($youtube_thumbnail);
+			curl_setopt($ch, CURLOPT_NOBODY, true);
+			curl_exec($ch);
+			$retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			// $retcode > 400 -> not found, $retcode = 200, found.
+			curl_close($ch);
+			if($retcode==200) {
+				$new_thumbnail = $youtube_thumbnail;
+			}
+		}
+		
+		// Vimeo
+		if($new_thumbnail==null) {
+		
+			// Standard embed code
+			preg_match('#<object[^>]+>.+?http://vimeo.com/moogaloop.swf\?clip_id=([A-Za-z0-9\-_]+)&.+?</object>#s', $markup, $matches);
+			
+			// Find Vimeo embedded with iframe code
+			if(!isset($matches[1])) {
+				preg_match('#http://player.vimeo.com/video/([0-9]+)#s', $markup, $matches);
+			}
+			
+			// If we still haven't found anything, check for Vimeo embedded with JR_embed
+			if(!isset($matches[1])) {
+		    	preg_match('#\[vimeo id=([A-Za-z0-9\-_]+)]#s', $markup, $matches);
+		    }
+	
+			// If we still haven't found anything, check for Vimeo URL
+			if(!isset($matches[1])) {
+		    	preg_match('#http://w?w?w?.?vimeo.com/([A-Za-z0-9\-_]+)#s', $markup, $matches);
+		    }
+	
+			// If we still haven't found anything, check for Vimeo shortcode
+			if(!isset($matches[1])) {
+		    	preg_match('#\[vimeo clip_id="([A-Za-z0-9\-_]+)"[^>]*]#s', $markup, $matches);
+		    }
+			if(!isset($matches[1])) {
+		    	preg_match('#\[vimeo video_id="([A-Za-z0-9\-_]+)"[^>]*]#s', $markup, $matches);
+		    }
+		
+			// Now if we've found a Vimeo ID, let's set the thumbnail URL
+			if(isset($matches[1])) {
+				$vimeo_thumbnail = getVimeoInfo($matches[1], $info = 'thumbnail_large');
+				if(isset($vimeo_thumbnail)) {
+					$new_thumbnail = $vimeo_thumbnail;
+				}
+			}
+		}
+		
+		// Blip.tv
+		if($new_thumbnail==null) {
+		
+			// Blip.tv file URL
+			preg_match('#http://blip.tv/file/([0-9]+)#s', $markup, $matches);
+
+			// Now if we've found a Blip.tv file URL, let's set the thumbnail URL
+			if(isset($matches[1])) {
+				$blip_thumbnail = getBliptvInfo($matches[1]);
+				$new_thumbnail = $blip_thumbnail;
+			}
+		}
+		
+		// Return the new thumbnail variable and update meta if one is found
+		if($new_thumbnail!=null) {
+			if(!update_post_meta($post_id, '_video_thumbnail', $new_thumbnail)) add_post_meta($post_id, '_video_thumbnail', $new_thumbnail);
+		}
+		return $new_thumbnail;
+
+	}
+};
+
+// Echo thumbnail
+function video_thumbnail($post_id=null) {
+	if( ( $video_thumbnail = get_video_thumbnail($post_id) ) == null ) { echo plugins_url() . "/video-thumbnails/default.jpg"; }
+	else { echo $video_thumbnail; }
+};
+
+// Create Meta Fields on Edit Page
+
+add_action("admin_init", "admin_init");
+ 
+function admin_init(){
+	add_meta_box("video_thumbnail", "Video Thumbnail", "video_thumbnail_admin", "post", "side", "low");
+}
+ 
+function video_thumbnail_admin(){
+	global $post;
+	$custom = get_post_custom($post->ID);
+	$video_thumbnail = $custom["_video_thumbnail"][0];
+	?>
+	<p><label>Video Thumbnail URL:</label></p>
+	<p><input type="text" size="16" name="video_thumbnail" style="width:250px;" value="<?php echo $video_thumbnail; ?>" /></p>
+	<?php if(isset($video_thumbnail) && $video_thumbnail!='') { ?><p><img src="<?php echo $video_thumbnail; ?>" width="100%" /></p><?php } ?>
+	<?php
+}
+
+// Save Meta Details
+
+add_action('save_post', 'save_details');
+
+function save_details(){
+	global $post;
+	if(isset($_POST["video_thumbnail"]) && $_POST["video_thumbnail"]!='') {
+		if(!update_post_meta($post->ID, "_video_thumbnail", $_POST["video_thumbnail"])) add_post_meta($post->ID, "_video_thumbnail", $_POST["video_thumbnail"]);
+	}
+	if(isset($_POST["video_thumbnail"]) && $_POST["video_thumbnail"]=='') {
+		delete_post_meta($post->ID, "_video_thumbnail");
+	}
+}
+
+?>
