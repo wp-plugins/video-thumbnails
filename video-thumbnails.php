634cea5b59e0d113b228cea382c1c917d9889b48
sutherlandboswell <sutherlandboswell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-19 18:12:07 +0000

    Fix for AYVP featured image bug
    git-svn-id: https://plugins.svn.wordpress.org/video-thumbnails/trunk@758646 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- video-thumbnails.php
+++ video-thumbnails.php
@@ -5,7 +5,7 @@ Plugin URI: http://refactored.co/plugins/video-thumbnails
 Description: Automatically retrieve video thumbnails for your posts and display them in your theme. Currently supports YouTube, Vimeo, Facebook, Blip.tv, Justin.tv, Dailymotion, Metacafe, Wistia, Youku, Funny or Die, and MPORA.
 Author: Sutherland Boswell
 Author URI: http://sutherlandboswell.com
-Version: 2.0.6
+Version: 2.0.7
 License: GPL2
 */
 /*  Copyright 2013 Sutherland Boswell  (email : sutherland.boswell@gmail.com)
@@ -28,7 +28,7 @@ License: GPL2
 
 define( 'VIDEO_THUMBNAILS_PATH', dirname(__FILE__) );
 define( 'VIDEO_THUMBNAILS_FIELD', '_video_thumbnail' );
-define( 'VIDEO_THUMBNAILS_VERSION', '2.0.6' );
+define( 'VIDEO_THUMBNAILS_VERSION', '2.0.7' );
 
 // Providers
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-video-thumbnails-providers.php' );
@@ -58,12 +58,7 @@ class Video_Thumbnails {
 		add_action( 'admin_init', array( &$this, 'meta_box_init' ) );
 
 		// Add actions to save video thumbnails when saving
-		add_action( 'new_to_draft', array( &$this, 'save_video_thumbnail' ), 10, 1 );
-		add_action( 'new_to_publish', array( &$this, 'save_video_thumbnail' ), 10, 1 );
-		add_action( 'draft_to_publish', array( &$this, 'save_video_thumbnail' ), 10, 1 );
-		add_action( 'pending_to_publish', array( &$this, 'save_video_thumbnail' ), 10, 1 );
-		add_action( 'future_to_publish', array( &$this, 'save_video_thumbnail' ), 10, 1 );
-		add_action( 'private_to_publish', array( &$this, 'save_video_thumbnail' ), 10, 1 );
+		add_action( 'save_post', array( &$this, 'save_video_thumbnail' ), 100, 1 );
 
 		// Add actions to save video thumbnails when posting from XML-RPC (this action passes the post ID as an argument so 'get_video_thumbnail' is used instead)
 		add_action( 'xmlrpc_publish_post', 'get_video_thumbnail', 10, 1 );
@@ -165,8 +160,11 @@ class Video_Thumbnails {
 				if ( !update_post_meta( $post_id, VIDEO_THUMBNAILS_FIELD, $new_thumbnail ) ) add_post_meta( $post_id, VIDEO_THUMBNAILS_FIELD, $new_thumbnail, true );
 
 				// Set attachment as featured image if enabled
-				if ( $this->settings->options['set_featured'] == 1 && $this->settings->options['save_media'] == 1 && !get_post_thumbnail_id( $post_id ) ) {
-					set_post_thumbnail( $post_id, $attachment_id );
+				if ( $this->settings->options['set_featured'] == 1 && $this->settings->options['save_media'] == 1 ) {
+					// Make sure there isn't already a post thumbnail
+					if ( intval( get_post_thumbnail_id( $post_id ) ) === 0 ) {
+						set_post_thumbnail( $post_id, $attachment_id );
+					}
 				}
 			}
 			return $new_thumbnail;
@@ -174,18 +172,20 @@ class Video_Thumbnails {
 		}
 	}
 
-	// Runs when post is saved
-	function save_video_thumbnail( $post ) {
-		$post_type = get_post_type( $post->ID );
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-			return null;
+	/**
+	 * Gets a video thumbnail when a published post is saved
+	 * @param  int $post_id The post ID
+	 */
+	function save_video_thumbnail( $post_id ) {
+		// Don't save video thumbnails during autosave or for unpublished posts
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return null;
+		if ( get_post_status( $post_id ) != 'publish' ) return null;
+		// Check that Video Thumbnails are enabled for current post type
+		$post_type = get_post_type( $post_id );
+		if ( in_array( $post_type, (array) $this->settings->options['post_types'] ) || $post_type == $this->settings->options['post_types'] ) {
+			$this->get_video_thumbnail( $post_id );
 		} else {
-			// Check that Video Thumbnails are enabled for current post type
-			if ( in_array( $post_type, (array) $this->settings->options['post_types'] ) || $post_type == $this->settings->options['post_types'] ) {
-				get_video_thumbnail( $post->ID );
-			} else {
-				return null;
-			}
+			return null;
 		}
 	}
 
