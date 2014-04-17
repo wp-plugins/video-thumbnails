71fdcaf3c5920ddeae8b57d8e54bee8eb74e782c
sutherlandboswell <sutherlandboswell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-17 01:03:54 +0000

    Better WP Robot and AYVP support
    git-svn-id: https://plugins.svn.wordpress.org/video-thumbnails/trunk@895399 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- video-thumbnails.php
+++ video-thumbnails.php
@@ -5,7 +5,7 @@ Plugin URI: https://refactored.co/plugins/video-thumbnails
 Description: Automatically retrieve video thumbnails for your posts and display them in your theme. Supports YouTube, Vimeo, Facebook, Vine, Justin.tv, Twitch, Dailymotion, Metacafe, VK, Blip, Google Drive, Funny or Die, CollegeHumor, MPORA, Wistia, Youku, and Rutube.
 Author: Sutherland Boswell
 Author URI: http://sutherlandboswell.com
-Version: 2.7.4
+Version: 2.7.5
 License: GPL2
 Text Domain: video-thumbnails
 Domain Path: /languages/
@@ -30,13 +30,13 @@ Domain Path: /languages/
 
 define( 'VIDEO_THUMBNAILS_PATH', dirname(__FILE__) );
 define( 'VIDEO_THUMBNAILS_FIELD', '_video_thumbnail' );
-define( 'VIDEO_THUMBNAILS_VERSION', '2.7.4' );
+define( 'VIDEO_THUMBNAILS_VERSION', '2.7.5' );
 
 // Providers
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-video-thumbnails-providers.php' );
 
 // Extensions
-require_once( VIDEO_THUMBNAILS_PATH . '/php/extensions/class-video-thumbnails-extension.php' );
+require_once( VIDEO_THUMBNAILS_PATH . '/php/extensions/extensions.php' );
 
 // Settings
 require_once( VIDEO_THUMBNAILS_PATH . '/php/class-video-thumbnails-settings.php' );
