4edbccf984774d91f7ae9cdfb38e99375a96601d
sutherlandboswell <sutherlandboswell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-04 20:31:37 +0000

    Added SAPO and Tudou
    git-svn-id: https://plugins.svn.wordpress.org/video-thumbnails/trunk@926743 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/providers/class-sapo-thumbnails.php
+++ php/providers/class-sapo-thumbnails.php
@@ -1,6 +1,6 @@
 <?php
 
-/*  Copyright 2013 Sutherland Boswell  (email : sutherland.boswell@gmail.com)
+/*  Copyright 2014 Sutherland Boswell  (email : sutherland.boswell@gmail.com)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License, version 2, as 
@@ -19,14 +19,14 @@
 // Require thumbnail provider class
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-video-thumbnails-providers.php' );
 
-class Justintv_Thumbnails extends Video_Thumbnails_Providers {
+class SAPO_Thumbnails extends Video_Thumbnails_Providers {
 
 	// Human-readable name of the video provider
-	public $service_name = 'Justin.tv';
-	const service_name = 'Justin.tv';
+	public $service_name = 'SAPO';
+	const service_name = 'SAPO';
 	// Slug for the video provider
-	public $service_slug = 'justintv';
-	const service_slug = 'justintv';
+	public $service_slug = 'sapo';
+	const service_slug = 'sapo';
 
 	public static function register_provider( $providers ) {
 		$providers[self::service_slug] = new self;
@@ -35,18 +35,26 @@ class Justintv_Thumbnails extends Video_Thumbnails_Providers {
 
 	// Regex strings
 	public $regexes = array(
-	    '#//(?:www\.)?justin\.tv/swflibs/JustinPlayer\.swf\?channel=([a-zA-Z0-9_]+)#' // Channel player
+		'#videos\.sapo\.pt/([A-Za-z0-9]+)/mov#', // iFrame SRC
 	);
 
 	// Thumbnail URL
 	public function get_thumbnail_url( $id ) {
-		$request = "http://api.justin.tv/api/channel/show/$id.json";
+		$request = "http://videos.sapo.pt/$id";
 		$response = wp_remote_get( $request, array( 'sslverify' => false ) );
 		if( is_wp_error( $response ) ) {
 			$result = $this->construct_info_retrieval_error( $request, $response );
 		} else {
-			$result = json_decode( $response['body'] );
-			$result = $result->screen_cap_url_huge;
+			$doc = new DOMDocument();
+			@$doc->loadHTML( $response['body'] );
+			$metas = $doc->getElementsByTagName( 'meta' );
+			for ( $i = 0; $i < $metas->length; $i++ ) {
+				$meta = $metas->item( $i );
+				if ( $meta->getAttribute( 'property' ) == 'og:image' ) {
+					$result = $meta->getAttribute( 'content' );
+					break;
+				}
+			}
 		}
 		return $result;
 	}
@@ -55,10 +63,10 @@ class Justintv_Thumbnails extends Video_Thumbnails_Providers {
 	public static function get_test_cases() {
 		return array(
 			array(
-				'markup'        => '<object type="application/x-shockwave-flash" data="http://www.justin.tv/swflibs/JustinPlayer.swf?channel=twit" id="live_embed_player_flash" height="300" width="400" bgcolor="#000000"><param name="allowFullScreen" value="true"/><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.justin.tv/swflibs/JustinPlayer.swf" /><param name="flashvars" value="hostname=www.justin.tv&channel=twit&auto_play=false&start_volume=25" /></object>',
-				'expected'      => 'http://static-cdn.jtvnw.net/previews/live_user_twit-630x473.jpg',
-				'expected_hash' => 'b8c0dd6565f34e6bfbbddbb07ff0df74',
-				'name'          => __( 'Flash Embed', 'video-thumbnails' )
+				'markup'        => '<iframe src="http://rd3.videos.sapo.pt/playhtml?file=http://rd3.videos.sapo.pt/ddACsFSuDEZZRWfNHTTy/mov/1" frameborder="0" scrolling="no" width="640" height="360" webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>',
+				'expected'      => 'http://cache12.stormap.sapo.pt/vidstore14/thumbnais/74/5f/4c/7038488_tf9s9.jpg',
+				'expected_hash' => '0f95b2d32f3989a5d10d4d249f40b989',
+				'name'          => __( 'iFrame Embed', 'video-thumbnails' )
 			),
 		);
 	}
@@ -66,6 +74,6 @@ class Justintv_Thumbnails extends Video_Thumbnails_Providers {
 }
 
 // Add to provider array
-add_filter( 'video_thumbnail_providers', array( 'Justintv_Thumbnails', 'register_provider' ) );
+add_filter( 'video_thumbnail_providers', array( 'SAPO_Thumbnails', 'register_provider' ) );
 
 ?>
