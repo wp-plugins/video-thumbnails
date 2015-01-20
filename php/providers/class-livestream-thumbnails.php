22fdb35c822159399d283f96f7933edb6d1d3c63
sutherlandboswell <sutherlandboswell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 23:53:05 +0000

    Livestream support. TED fix.
    git-svn-id: https://plugins.svn.wordpress.org/video-thumbnails/trunk@1072062 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/providers/class-livestream-thumbnails.php
+++ php/providers/class-livestream-thumbnails.php
@@ -1,6 +1,6 @@
 <?php
 
-/*  Copyright 2014 Sutherland Boswell  (email : sutherland.boswell@gmail.com)
+/*  Copyright 2015 Sutherland Boswell  (email : sutherland.boswell@gmail.com)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License, version 2, as 
@@ -19,14 +19,14 @@
 // Require thumbnail provider class
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-video-thumbnails-provider.php' );
 
-class Ted_Thumbnails extends Video_Thumbnails_Provider {
+class Livestream_Thumbnails extends Video_Thumbnails_Provider {
 
 	// Human-readable name of the video provider
-	public $service_name = 'TED';
-	const service_name = 'TED';
+	public $service_name = 'Livestream';
+	const service_name = 'Livestream';
 	// Slug for the video provider
-	public $service_slug = 'ted';
-	const service_slug = 'ted';
+	public $service_slug = 'livestream';
+	const service_slug = 'livestream';
 
 	public static function register_provider( $providers ) {
 		$providers[self::service_slug] = new self;
@@ -35,19 +35,12 @@ class Ted_Thumbnails extends Video_Thumbnails_Provider {
 
 	// Regex strings
 	public $regexes = array(
-		'#//embed(?:\-ssl)?\.ted\.com/talks/([A-Za-z0-9_-]+)\.html#', // iFrame SRC
+		'#\/\/cdn\.livestream\.com\/embed\/([A-Za-z0-9_]+)#', // Embed SRC
 	);
 
 	// Thumbnail URL
 	public function get_thumbnail_url( $id ) {
-		$request = "http://www.ted.com/talks/oembed.json?url=http%3A%2F%2Fwww.ted.com%2Ftalks%2F$id";
-		$response = wp_remote_get( $request );
-		if( is_wp_error( $response ) ) {
-			$result = $this->construct_info_retrieval_error( $request, $response );
-		} else {
-			$result = json_decode( $response['body'] );
-			$result = str_replace( '240x180.jpg', '480x360.jpg', $result->thumbnail_url );
-		}
+		$result = 'http://thumbnail.api.livestream.com/thumbnail?name=' . $id;
 		return $result;
 	}
 
@@ -55,9 +48,9 @@ class Ted_Thumbnails extends Video_Thumbnails_Provider {
 	public static function get_test_cases() {
 		return array(
 			array(
-				'markup'        => '<iframe src="http://embed.ted.com/talks/kitra_cahana_stories_of_the_homeless_and_hidden.html" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
-				'expected'      => 'http://images.ted.com/images/ted/341053090f8bac8c324c75be3114b673b4355e8a_480x360.jpg',
-				'expected_hash' => 'f2a5f6af49e841b4f9c7b95d6ca0372a',
+				'markup'        => '<iframe width="560" height="340" src="http://cdn.livestream.com/embed/WFMZ_Traffic?layout=4&amp;height=340&amp;width=560&amp;autoplay=false" style="border:0;outline:0" frameborder="0" scrolling="no"></iframe>',
+				'expected'      => 'http://thumbnail.api.livestream.com/thumbnail?name=WFMZ_Traffic',
+				'expected_hash' => '1be02799b2fab7a4749b2187f7687412',
 				'name'          => __( 'iFrame Embed', 'video-thumbnails' )
 			),
 		);
