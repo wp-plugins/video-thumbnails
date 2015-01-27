a0bad34aa83833f9d662a996334229c797a6509d
sutherlandboswell <sutherlandboswell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-27 02:49:38 +0000

    Added Yahoo Screen support
    git-svn-id: https://plugins.svn.wordpress.org/video-thumbnails/tags/2.12@1076402 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/providers/class-dailymotion-thumbnails.php
+++ php/providers/class-dailymotion-thumbnails.php
@@ -1,6 +1,6 @@
 <?php
 
-/*  Copyright 2013 Sutherland Boswell  (email : sutherland.boswell@gmail.com)
+/*  Copyright 2014 Sutherland Boswell  (email : sutherland.boswell@gmail.com)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License, version 2, as 
@@ -17,9 +17,9 @@
 */
 
 // Require thumbnail provider class
-require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-video-thumbnails-providers.php' );
+require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-video-thumbnails-provider.php' );
 
-class Dailymotion_Thumbnails extends Video_Thumbnails_Providers {
+class Dailymotion_Thumbnails extends Video_Thumbnails_Provider {
 
 	// Human-readable name of the video provider
 	public $service_name = 'Dailymotion';
@@ -36,14 +36,14 @@ class Dailymotion_Thumbnails extends Video_Thumbnails_Providers {
 	// Regex strings
 	public $regexes = array(
 		'#<object[^>]+>.+?http://www\.dailymotion\.com/swf/video/([A-Za-z0-9]+).+?</object>#s', // Dailymotion flash
-		'#https?://www\.dailymotion\.com/embed/video/([A-Za-z0-9]+)#', // Dailymotion iframe
+		'#//www\.dailymotion\.com/embed/video/([A-Za-z0-9]+)#', // Dailymotion iframe
 		'#(?:https?://)?(?:www\.)?dailymotion\.com/video/([A-Za-z0-9]+)#' // Dailymotion URL
 	);
 
 	// Thumbnail URL
 	public function get_thumbnail_url( $id ) {
 		$request = "https://api.dailymotion.com/video/$id?fields=thumbnail_url";
-		$response = wp_remote_get( $request, array( 'sslverify' => false ) );
+		$response = wp_remote_get( $request );
 		if( is_wp_error( $response ) ) {
 			$result = $this->construct_info_retrieval_error( $request, $response );
 		} else {
@@ -72,8 +72,5 @@ class Dailymotion_Thumbnails extends Video_Thumbnails_Providers {
 	}
 
 }
-
-// Add to provider array
-add_filter( 'video_thumbnail_providers', array( 'Dailymotion_Thumbnails', 'register_provider' ) );
 
 ?>
--- php/providers/class-sapo-thumbnails.php
+++ php/providers/class-sapo-thumbnails.php
@@ -19,14 +19,14 @@
 // Require thumbnail provider class
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-video-thumbnails-provider.php' );
 
-class Kaltura_Thumbnails extends Video_Thumbnails_Provider {
+class Sapo_Thumbnails extends Video_Thumbnails_Provider {
 
 	// Human-readable name of the video provider
-	public $service_name = 'Kaltura';
-	const service_name = 'Kaltura';
+	public $service_name = 'SAPO';
+	const service_name = 'SAPO';
 	// Slug for the video provider
-	public $service_slug = 'kaltura';
-	const service_slug = 'kaltura';
+	public $service_slug = 'sapo';
+	const service_slug = 'sapo';
 
 	public static function register_provider( $providers ) {
 		$providers[self::service_slug] = new self;
@@ -35,25 +35,27 @@ class Kaltura_Thumbnails extends Video_Thumbnails_Provider {
 
 	// Regex strings
 	public $regexes = array(
-	    '#http://cdnapi\.kaltura\.com/p/[0-9]+/sp/[0-9]+/embedIframeJs/uiconf_id/[0-9]+/partner_id/[0-9]+\?entry_id=([a-z0-9_]+)#' // Hosted
+		'#videos\.sapo\.pt/([A-Za-z0-9]+)/mov#', // iFrame SRC
 	);
 
 	// Thumbnail URL
 	public function get_thumbnail_url( $id ) {
-		$request = "http://www.kaltura.com/api_v3/?service=thumbAsset&action=getbyentryid&entryId=$id";
+		$request = "http://videos.sapo.pt/$id";
 		$response = wp_remote_get( $request );
 		if( is_wp_error( $response ) ) {
 			$result = $this->construct_info_retrieval_error( $request, $response );
 		} else {
-			$xml = new SimpleXMLElement( $response['body'] );
-			$result = (string) $xml->result->item->id;
-			$request = "http://www.kaltura.com/api_v3/?service=thumbAsset&action=geturl&id=$result";
-			$response = wp_remote_get( $request );
-			if( is_wp_error( $response ) ) {
-				$result = $this->construct_info_retrieval_error( $request, $response );
-			} else {
-				$xml = new SimpleXMLElement( $response['body'] );
-				$result = (string) $xml->result;
+			$doc = new DOMDocument();
+			@$doc->loadHTML( $response['body'] );
+			$metas = $doc->getElementsByTagName( 'meta' );
+			for ( $i = 0; $i < $metas->length; $i++ ) {
+				$meta = $metas->item( $i );
+				if ( $meta->getAttribute( 'property' ) == 'og:image' ) {
+					$og_image = $meta->getAttribute( 'content' );
+					parse_str( parse_url( $og_image, PHP_URL_QUERY ), $image_array );
+					$result = $image_array['pic'];
+					break;
+				}
 			}
 		}
 		return $result;
@@ -63,9 +65,10 @@ class Kaltura_Thumbnails extends Video_Thumbnails_Provider {
 	public static function get_test_cases() {
 		return array(
 			array(
-				'markup'   => '<script type="text/javascript" src="http://cdnapi.kaltura.com/p/1374841/sp/137484100/embedIframeJs/uiconf_id/12680902/partner_id/1374841?entry_id=1_y7xzqsxw&playerId=kaltura_player_1363589321&cache_st=1363589321&autoembed=true&width=400&height=333&"></script>',
-				'expected' => 'http://example.com/thumbnail.jpg',
-				'name'     => __( 'Auto Embed', 'video-thumbnails' )
+				'markup'        => '<iframe src="http://rd3.videos.sapo.pt/playhtml?file=http://rd3.videos.sapo.pt/ddACsFSuDEZZRWfNHTTy/mov/1" frameborder="0" scrolling="no" width="640" height="360" webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>',
+				'expected'      => 'http://cache02.stormap.sapo.pt/vidstore14/thumbnais/e9/08/37/7038489_l5VMt.jpg',
+				'expected_hash' => 'd8a74c3d4e054263a37abe9ceed782fd',
+				'name'          => __( 'iFrame Embed', 'video-thumbnails' )
 			),
 		);
 	}
